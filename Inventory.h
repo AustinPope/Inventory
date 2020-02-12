24e6d5a287ccb03dc30212b46168bebcf1e38a75
AustinPope <mraustinpope@gmail.com>
2020-02-12 11:22:18 -0800

    Documentation for class header

--- Inventory.h
+++ Inventory.h
@@ -5,6 +5,16 @@
 using std::string;
 using std::ostream;
 
+
+
+
+
+/* A class keeping track of the store's stock.
+ * For each item in stock we are keeping track of a name, price, and if in stock
+ *
+ * A function for contructing, selling, and something to do with a good friend.
+ */
+
 class Inventory
 {
   private:
