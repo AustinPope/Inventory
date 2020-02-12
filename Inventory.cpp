44855c70551ca0a6f0d61c1601ee5897b2a130be
AustinPope <mraustinpope@gmail.com>
2020-02-12 11:23:09 -0800

    Documentation for class functions

--- Inventory.cpp
+++ Inventory.cpp
@@ -5,6 +5,17 @@
 using std::string;
 using std::ostream;
   
+/* Contructor for class
+ *
+ * Takes in some variables when there is a class instance created. These variables
+ * are to be copied over to their respected variables within the class.
+ *
+ * @param a string name
+ * @param a float price
+ * @param an int count
+ *
+ * @return Nothing
+ */
 Inventory::Inventory(string name, float price, int count)
 {
   m_name = name;
@@ -12,11 +23,24 @@ Inventory::Inventory(string name, float price, int count)
   m_in_stock = count;
 }
 
+/* Whenever we sell an item, we decrement how many are in stock.
+ *
+ * When we sell something that is in store, we want to keep track of the record
+ * so we know how many we have left. That is so we do not oversell and we know
+ * when we have to order more. We decrememnt that class variable associated 
+ * with this
+ *
+ * @param none
+ *
+ * @return nothing
+ */
 void Inventory::sell()
 {
   m_in_stock--;
 }
 
+
+//Ill be honest I do not know whats going on with this jazz.
 ostream& operator<<(ostream& stream, const Inventory& item)
 {
   stream << item.m_name 
